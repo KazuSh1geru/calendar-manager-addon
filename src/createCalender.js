81539f62d2f09f4bf66417e4ffa6faef2553d931
Takakusagi Kazushi <kusagi@TakakusaginoMBP.usen.ad.jp>
2023-09-24 20:22:13 +0900

    リファクタリング”

--- src/createCalender.js
+++ src/createCalender.js
@@ -1,22 +1,28 @@
 function handleButtonClick(e) {
-     // スクリプトのタイムゾーンで日時を解析
-     var timeZone = Session.getScriptTimeZone();
+	// スクリプトのタイムゾーンで日時を解析
+	var timeZone = Session.getScriptTimeZone();
 
 	var startMsSinceEpoch = e.formInput.startDatetime.msSinceEpoch;
-    Logger.log('startMsSinceEpoch: ' + startMsSinceEpoch);
-	var startDatetime = new Date(startMsSinceEpoch);
-    Logger.log('startDatetime: ' + startDatetime);
-	// YYYY-MM-DDTHH:MM:SS 形式に変換
-    var startFormattedDate = Utilities.formatDate(startDatetime, timeZone, "yyyy-MM-dd'T'HH:mm:ss");
-    Logger.log('startFormattedDate: ' + startFormattedDate);
+	var startFormattedDatetime = getFormattedDatetime(
+		startMsSinceEpoch,
+		timeZone,
+	);
 
 	var endMsSinceEpoch = e.formInput.endDatetime.msSinceEpoch;
-	var endDatetime = new Date(endMsSinceEpoch);
-	// YYYY-MM-DD 形式に変換
-	var endFormattedDate = Utilities.formatDate(endDatetime, timeZone, "yyyy-MM-dd'T'HH:mm:ss");
+	var endFormattedDatetime = getFormattedDatetime(endMsSinceEpoch, timeZone);
 
-	Logger.log('startDatetime: ' + startFormattedDate);
-	createEvent(startFormattedDate, endFormattedDate);
+	createEvent(startFormattedDatetime, endFormattedDatetime);
+}
+
+function getFormattedDatetime(msSinceEpoch, timeZone) {
+	var datetime = new Date(msSinceEpoch);
+	// YYYY-MM-DDTHH:MM:SS 形式に変換
+	var formattedDatetime = Utilities.formatDate(
+		datetime,
+		timeZone,
+		"yyyy-MM-dd'T'HH:mm:ss",
+	);
+	return formattedDatetime;
 }
 
 // Creates an all-day event for the moon landing and logs the ID.
@@ -29,10 +35,8 @@ function createEvent(startDatetime, endDatetime) {
 	Logger.log('startDatetime: ' + startDatetime);
 	Logger.log('endDatetime: ' + endDatetime);
 	var event = CalendarApp.getDefaultCalendar().createEvent(
-		title='テストイベント',
-		startTime=new Date(startDatetime),
-        endTime=new Date(endDatetime),
+		(title = 'テストイベント'),
+		(startTime = new Date(startDatetime)),
+		(endTime = new Date(endDatetime)),
 	);
-	Logger.log('Event ID: ' + event.getId());
-	Logger.log('Event End Time: ' + event.getEndTime());
 }
