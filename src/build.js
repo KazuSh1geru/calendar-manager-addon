b31643b3514ca1c19486a0a5985f50f2a0d84d3e
Takakusagi Kazushi <kusagi@TakakusaginoMBP.usen.ad.jp>
2023-09-24 17:28:09 +0900

    make lint

--- src/build.js
+++ src/build.js
@@ -1,26 +1,24 @@
 function buildCard() {
-    let cardSection1TextParagraph1 = CardService.newTextParagraph()
-        .setText('デモアプリです!');
+	let cardSection1TextParagraph1 =
+		CardService.newTextParagraph().setText('デモアプリです!');
 
-    let cardSection1DatePicker1 = CardService.newDatePicker()
-        .setFieldName('date')
-        .setTitle('日付を指定できるよ！');
+	let cardSection1DatePicker1 = CardService.newDatePicker()
+		.setFieldName('date')
+		.setTitle('日付を指定できるよ！');
 
-    let cardSection1TimePicker1 = CardService.newTimePicker()
-        .setFieldName('Time')
-        .setTitle('時間を指定できるよ！');
+	let cardSection1TimePicker1 = CardService.newTimePicker()
+		.setFieldName('Time')
+		.setTitle('時間を指定できるよ！');
 
-    let cardSection1 = CardService.newCardSection()
-        .addWidget(cardSection1TextParagraph1)
-        .addWidget(cardSection1DatePicker1)
-        .addWidget(cardSection1TimePicker1);
+	let cardSection1 = CardService.newCardSection()
+		.addWidget(cardSection1TextParagraph1)
+		.addWidget(cardSection1DatePicker1)
+		.addWidget(cardSection1TimePicker1);
 
-    let card = CardService.newCardBuilder()
-        .addSection(cardSection1)
-        .build();
-    return card;
+	let card = CardService.newCardBuilder().addSection(cardSection1).build();
+	return card;
 }
 
 function onDefaultHomePageOpen() {
-    return buildCard()
+	return buildCard();
 }
