6b9aa26b44c341c15e1eea1c23eb77c885fed38a
Takakusagi Kazushi <kusagi@TakakusaginoMacBook-Pro.local>
2023-11-12 21:19:04 +0900

    lint実行

--- src/domain/schedule.ts
+++ src/domain/schedule.ts
@@ -1,34 +1,32 @@
 interface Schedule {
-	title: string;
-	startDatetime: Date;
-	endDatetime: Date;
+  title: string;
+  startDatetime: Date;
+  endDatetime: Date;
 }
 
 interface ScheduleRepository {
-	create(schedule: Schedule): Schedule | undefined;
-	update(schedule: Schedule): Schedule;
+  create(schedule: Schedule): Schedule | undefined;
+  update(schedule: Schedule): Schedule;
 }
 
 class DefaultScheduleRepository implements ScheduleRepository {
-	create(schedule: Schedule): Schedule | undefined {
-		const startDatetime = schedule.startDatetime;
-		const endDatetime = schedule.endDatetime;
-		if (!startDatetime || !endDatetime) {
-			// 日付が設定されていない場合、エラーを回避
-			Logger.log('日付が設定されていません。');
-			return;
-		}
-		// Logger.log('startDatetime: ' + startDatetime);
-		// Logger.log('endDatetime: ' + endDatetime);
-		const event = CalendarApp.getDefaultCalendar().createEvent(
-			'テストイベント',
-			startDatetime,
-			endDatetime,
-		);
-		return schedule;
-	}
+  create(schedule: Schedule): Schedule | undefined {
+    const startDatetime = schedule.startDatetime;
+    const endDatetime = schedule.endDatetime;
+    if (!startDatetime || !endDatetime) {
+      // 日付が設定されていない場合、エラーを回避
+      console.log('日付が設定されていません。');
+      return;
+    }
+    const event = CalendarApp.getDefaultCalendar().createEvent(
+      'テストイベント',
+      startDatetime,
+      endDatetime,
+    );
+    return schedule;
+  }
 
-	update(schedule: Schedule): Schedule {
-		return schedule;
-	}
+  update(schedule: Schedule): Schedule {
+    return schedule;
+  }
 }
--- src/handler/handleCreateSchedule.ts
+++ src/handler/handleCreateSchedule.ts
@@ -1,56 +1,47 @@
-// 必要に応じて、以下の型や関数の定義を調整してください。
-declare const Session: any;
-declare const Utilities: any;
-declare const Logger: any;
-declare const CalendarApp: any;
+type HandleCreateScheduleActionEvent = ActionEvent<
+  {
+    startDatetime: Date;
+    endDatetime: Date;
+  },
+  {}
+>;
 
-type FormInput = {
-	startDatetime: { msSinceEpoch: number };
-	endDatetime: { msSinceEpoch: number };
+class CreateScheduleHandler extends BaseActionHandler<HandleCreateScheduleActionEvent> {
+  constructor(private scheduleRepository: ScheduleRepository) {
+    super();
+  }
+
+  handleEvent(e: HandleCreateScheduleActionEvent): Schedule | undefined {
+    const schedule = this.toSchedule(e);
+    const createdSchedule = this.scheduleRepository.create(schedule);
+    return createdSchedule;
+  }
+
+  private toSchedule(e: HandleCreateScheduleActionEvent): Schedule {
+    console.log('HandleCreateScheduleActionEvent', e);
+    const formInputs = e.commonEventObject.formInputs;
+    const startMsSinceEpoch =
+      formInputs.startDatetime.dateTimeInput.msSinceEpoch;
+    const endMsSinceEpoch = formInputs.endDatetime.dateTimeInput.msSinceEpoch;
+    const startDatetime = this.toDate(startMsSinceEpoch);
+    const endDatetime = this.toDate(endMsSinceEpoch);
+    const schedule = {
+      title: 'テストイベント',
+      startDatetime: startDatetime,
+      endDatetime: endDatetime,
+    };
+    console.log('schedule', schedule);
+    return schedule;
+  }
+  private toDate(msSinceEpoch: number): Date {
+    const date = new Date(msSinceEpoch);
+    return date;
+  }
 }
 
-interface ActionEventObject {
-	formInput: FormInput,
-}
-
-class CreateScheduleHandler<ActionEventObject> {
-	constructor(
-		private scheduleRepository: ScheduleRepository,
-		private timeZone: string,
-	) {
-		this.timeZone = Session.getScriptTimeZone();
-	}
-
-	handleEvent(e: ActionEventObject): Schedule | undefined {
-		const schedule = this.toSchedule(e);
-		const createdSchedule = this.scheduleRepository.create(schedule);
-		return createdSchedule;
-	}
-
-	private toSchedule(e: ActionEventObject): Schedule {
-		const startMsSinceEpoch = e.formInput.startDatetime.msSinceEpoch;
-		const endMsSinceEpoch = e.formInput.endDatetime.msSinceEpoch;
-		const startDatetime = this.toDate(
-			startMsSinceEpoch,
-		);
-		const endDatetime = this.toDate(
-			endMsSinceEpoch,
-		);
-		return {
-			title: 'テストイベント',
-			startDatetime: startDatetime,
-			endDatetime: endDatetime,
-		};
-	}
-	private toDate(msSinceEpoch: number): Date {
-		const date = new Date(msSinceEpoch);
-		return date;
-	}
-}
-
-function handleCreateSchedule(e: ActionEventObject) {
-	const scheduleRepository = new DefaultScheduleRepository();
-	const handler = new CreateScheduleHandler(scheduleRepository, Session.getScriptTimeZone());
-	const schedule = handler.handleEvent(e);
-	return schedule;
+function handleCreateSchedule(e: HandleCreateScheduleActionEvent) {
+  const scheduleRepository = new DefaultScheduleRepository();
+  const handler = new CreateScheduleHandler(scheduleRepository);
+  const schedule = handler.handleEvent(e);
+  return schedule;
 }
